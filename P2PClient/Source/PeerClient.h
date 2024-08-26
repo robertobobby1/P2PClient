67071a3533308d1518d255eedf40279a20228502
Roberto Milla Martinez <roberto@accounton.io>
2024-08-26 02:08:41 +0200

    feat: updated R version

--- P2PClient/Source/PeerClient.h
+++ P2PClient/Source/PeerClient.h
@@ -1,36 +1,12 @@
 #pragma once
 
 #include "R.h"
-#include "NetworkStructs.h"
 
 namespace PeerClient {
     inline const int SECURITY_HEADER_LENGTH = 23;
     inline const char* SECURITY_HEADER = "0sdFGeVi3ItN1qwsHp3mcDF";
 
     inline std::shared_ptr<R::Net::Client> client;
-
-    inline uint8_t createProtocolHeader(LobbyPrivacyType lobbyType, ActionType actionType) {
-        uint8_t headerFlags = 0;
-        if (lobbyType == LobbyPrivacyType::Public) {
-            R::Utils::setFlag(headerFlags, ClientServerHeaderFlags_Public);
-        }
-        switch (actionType) {
-            case ActionType::Connect:  // 11
-                R::Utils::setFlag(headerFlags, ClientServerHeaderFlags_Bit1);
-                R::Utils::setFlag(headerFlags, ClientServerHeaderFlags_Bit2);
-                break;
-            case ActionType::Create:  // 10
-                R::Utils::setFlag(headerFlags, ClientServerHeaderFlags_Bit1);
-                break;
-            case ActionType::Disconnect:  // 01
-                R::Utils::setFlag(headerFlags, ClientServerHeaderFlags_Bit2);
-                break;
-            case ActionType::PeerConnectSuccess:  // 00
-                break;
-        }
-
-        return headerFlags;
-    }
 
     inline void run(const char* hostname, int port) {
         client = R::Net::Client::makeAndRun(hostname, port);
@@ -39,11 +15,7 @@ namespace PeerClient {
             return;
         }
 
-        auto buffer = R::Buffer(SECURITY_HEADER_LENGTH + 1);
-        auto headerFlags = createProtocolHeader(LobbyPrivacyType::Public, ActionType::Create);
-
-        buffer.write(SECURITY_HEADER, SECURITY_HEADER_LENGTH);
-        buffer.write(headerFlags);
+        auto buffer = R::Net::P2P::createClientPublicConnectBuffer();
         client->sendMessage(buffer);
 
         auto readBuffer = client->readMessage();
--- P2PClient/Source/PeerServer.h
+++ P2PClient/Source/PeerServer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+
 #include "R.h"
 
 namespace PeerServer {
